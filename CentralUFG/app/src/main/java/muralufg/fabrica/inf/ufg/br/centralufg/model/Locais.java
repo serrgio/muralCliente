8d974c3eebc370e3ac7afa1cecd5387e50ed19fc
Douglas Japiassu <douglas.japiassu@gmail.com>
2014-10-23 21:33:30 -0200

    Implementação inicial do pacote Locais.

---
+++ CentralUFG/app/src/main/java/muralufg/fabrica/inf/ufg/br/centralufg/model/Locais.java
@@ -0,0 +1,124 @@
+/*
+ * ====================================================================
+ * Licença da Fábrica de Software (UFG)
+ *
+ * Copyright (c) 2014 Fábrica de Software
+ * Instituto de Informática (Universidade Federal de Goiás)
+ * Todos os direitos reservados.
+ *
+ * Redistribuição e uso, seja dos fontes ou do formato binário
+ * correspondente, com ou sem modificação, são permitidos desde que
+ * as seguintes condições sejam atendidas:
+ *
+ * 1. Redistribuição do código fonte deve conter esta nota, em sua
+ *    totalidade, ou seja, a nota de copyright acima, as condições
+ *    e a observação sobre garantia abaixo.
+ *
+ * 2. Redistribuição no formato binário deve reproduzir o conteúdo
+ *    desta nota, em sua totalidade, ou seja, o copyright acima,
+ *    esta lista de condições e a observação abaixo na documentação
+ *    e/ou materiais fornecidos com a distribuição.
+ *
+ * 3. A documentação fornecida com a redistribuição,
+ *    qualquer que seja esta, deve incluir o seguinte
+ *    texto, entre aspas:
+ *       "Este produto inclui software desenvolvido pela Fábrica
+ *       de Software do Instituto de Informática (UFG)."
+ *
+ * 4. Os nomes Fábrica de Software, Instituto de Informática e UFG
+ *    não podem ser empregados para endoçar ou promover produtos
+ *    derivados do presente software sem a explícita permissão
+ *    por escrito.
+ *
+ * 5. Produtos derivados deste software não podem ser chamados
+ *    "Fábrica de Software", "Instituto de Informática", "UFG",
+ *    "Universidade Federal de Goiás" ou contê-los em seus nomes,
+ *    sem permissão por escrito.
+ *
+ * ESTE SOFTWARE É FORNECIDO "COMO ESTÁ". NENHUMA GARANTIA É FORNECIDA,
+ * EXPLÍCITA OU NÃO. NÃO SE AFIRMA QUE O PRESENTE SOFTWARE
+ * É ADEQUADO PARA QUALQUER QUE SEJA O USO. DE FATO, CABE AO
+ * INTERESSADO E/OU USUÁRIO DO PRESENTE SOFTWARE, IMEDIATO OU NÃO,
+ * ESTA AVALIAÇÃO E A CONSEQUÊNCIA QUE O USO DELE OCASIONAR. QUALQUER
+ * DANO QUE DESTE SOFTWARE DERIVAR DEVE SER ATRIBUÍDO AO INTERESSADO
+ * E/OU USUÁRIO DESTE SOFTWARE.
+ * ====================================================================
+ *
+ * Este software é resultado do trabalho de voluntários, estudantes e
+ * professores, ao realizar atividades no âmbito da Fábrica de Software
+ * do Instituto de Informática (UFG). Consulte <http://fs.inf.ufg.br>
+ * para detalhes.
+ */
+
+package muralufg.fabrica.inf.ufg.br.centralufg.model;
+
+public class Locais {
+
+    private int id;
+    private String nome;
+    private String imagem;
+    private String endereco;
+    private String telefone;
+    private String localizacaoGeografica;
+
+    public Locais() {
+
+    }
+
+    public Locais(int id, String nome, String imagem, String endereco, String telefone, String localizacaoGeografica) {
+        this.id = id;
+        this.nome = nome;
+        this.imagem = imagem;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.localizacaoGeografica = localizacaoGeografica;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getLocalizacaoGeografica() {
+        return localizacaoGeografica;
+    }
+
+    public void setLocalizacaoGeografica(String localizacaoGeografica) {
+        this.localizacaoGeografica = localizacaoGeografica;
+    }
+}
