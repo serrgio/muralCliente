3dccabbd13d0f230025c1c399f4db3f4867f2df1
unknown <GAOliveira@INF04GO.gsa.go>
2014-10-23 20:27:45 -0200

    Criação do módulo cartões. Grupo Geovane, Iasmim e Kelves

---
+++ CentralUFG/app/src/main/java/muralufg/fabrica/inf/ufg/br/centralufg/util/view/cartao/ListaCartoesAdapter.java
@@ -0,0 +1,54 @@
+package muralufg.fabrica.inf.ufg.br.centralufg.util.view.cartao;
+
+/**
+ * Created by GAOliveira on 20/10/2014.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import muralufg.fabrica.inf.ufg.br.centralufg.R;
+
+public class ListaCartoesAdapter extends ArrayAdapter<Cartao> {
+
+    private Context context;
+    private List<Cartao> cartoes = null;
+
+    public ListaCartoesAdapter(Context context, List<Cartao> cartoes) {
+        super(context,0, cartoes);
+        this.cartoes = cartoes;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int posicao, View viewcartao, ViewGroup parent) {
+        Cartao cartao = this.cartoes.get(posicao);
+
+        if(viewcartao == null)
+            viewcartao = LayoutInflater.from(context).inflate(R.layout.item_list_cartao, null);
+
+        ImageView imageViewcartao = (ImageView) viewcartao.findViewById(R.id.image_view_cartao);
+        imageViewcartao.setImageResource(cartao.getImagem());
+
+        TextView textViewTituloCartao = (TextView) viewcartao.findViewById(R.id.text_view_titulo_cartao);
+        textViewTituloCartao.setText(cartao.getTitulo());
+
+        TextView textViewDataCartao = (TextView)viewcartao.findViewById(R.id.text_view_data_cartao);
+        textViewDataCartao.setText(cartao.getData());
+
+        TextView textViewDescricaoCartao = (TextView) viewcartao.findViewById(R.id.text_view_descricao_cartao);
+        textViewDescricaoCartao.setText(cartao.getDescricao());
+
+        return viewcartao;
+    }
+
+
+
+}
